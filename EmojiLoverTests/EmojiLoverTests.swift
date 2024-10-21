152ef6917472f5c62304cff2d7171985b4311e86
HungKai <107092995+Earnest3005@users.noreply.github.com>
2024-10-21 12:53:40 +0800

    Initial Commit

---
+++ EmojiLoverTests/EmojiLoverTests.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiLoverTests.swift
+//  EmojiLoverTests
+//
+//  Created by Earnest Hsiao on 2024/10/21.
+//
+
+import Testing
+@testable import EmojiLover
+
+struct EmojiLoverTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
